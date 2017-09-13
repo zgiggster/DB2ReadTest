4fc4e65c88dedccbfb8a968713b2a195e64d9886
zgiggster <zoran.gojceta@gmail.com>
2017-09-13 07:57:58 +0200

    Add project files.

---
+++ CatiaToCGR/CatiaApp.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INFITF;
+using MECMOD;
+using PARTITF;
+using KnowledgewareTypeLib;
+using HybridShapeTypeLib;
+using ProductStructureTypeLib;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Globalization;
+
+namespace CatiaToCGR
+{
+    class CatiaApp
+    {
+        const string CATIAapp = "CATIA.Application";
+        public INFITF.Application myCATIA { get; set; }
+
+
+        public string ExportToCgr(string path, string dir)
+        {
+            string dirName = path.Substring(0,path.LastIndexOf('\\'));
+            dirName = dirName.Substring(0, dirName.LastIndexOf('\\'));
+            dirName = dirName.Substring(dirName.LastIndexOf('\\') + 1);
+            string name = path.Substring(path.LastIndexOf('\\') + 1);
+            name = name.Substring(0, name.LastIndexOf('.'));
+
+            ProductDocument prodDoc = null;
+            try
+            {
+                prodDoc = (ProductDocument)myCATIA.Documents.Read(path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Path '" + path + "' could not be read.");
+                String errType = ex.GetType().Name;
+                if (errType == "TimeoutException")
+                {
+                    //TimeoutException e = ex as TimeoutException;
+                    Console.WriteLine("It's the TimeoutException !");
+                }
+                if (errType == "COMException")
+                {
+                    COMException COMex = ex as COMException;
+                    Console.WriteLine("COMerror " + COMex.ErrorCode.ToString() + ": " + COMex.Message);
+                }
+                if (prodDoc != null) prodDoc.Close();
+                return null;
+            }
+
+            string prodDocName = dir + "\\" + dirName + " - " + name;
+            if (System.IO.File.Exists(prodDocName + ".cgr"))
+                System.IO.File.Delete(prodDocName + ".cgr");
+            try
+            {
+                prodDoc.ExportData(prodDocName + ".CGR", "cgr");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Path '" + path + "' could not be exported.");
+                if (prodDoc != null) prodDoc.Close();
+                return null;
+            }
+
+            if (prodDoc != null) prodDoc.Close();
+            return prodDocName;
+        }
+
+
+        public void SaveCgrToProject(string docName)
+        {
+            Documents docs = myCATIA.Documents;
+            ProductDocument prodDoc = (ProductDocument)docs.Add("Product");
+            Product prod = prodDoc.Product;
+            Products prods = prod.Products;
+            Array arr = Array.CreateInstance(typeof(object), 1);
+            arr.SetValue(docName + ".cgr", 0);
+            prods.AddComponentsFromFiles(arr, "All");
+
+            string dir = docName.Substring(0, docName.LastIndexOf('\\') + 1);
+            string name = docName.Substring(docName.LastIndexOf('\\') + 1);
+            docName = dir + RemoveDiacritics(name);
+            prodDoc = (ProductDocument)myCATIA.ActiveDocument;
+            if (System.IO.File.Exists(docName + ".CATProduct"))
+                System.IO.File.Delete(docName + ".CATProduct");
+            try
+            {
+                prodDoc.SaveAs(docName + ".CATProduct");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("CGR document '" + docName + 
+                    "' could not be saved as CATProduct.");
+            }
+            if (prodDoc != null) prodDoc.Close();
+        }
+
+
+        public static string RemoveDiacritics(string str)
+        {
+            if (str == null) return null;
+            var chars =
+                from c in str.Normalize(NormalizationForm.FormD).ToCharArray()
+                let uc = CharUnicodeInfo.GetUnicodeCategory(c)
+                where uc != UnicodeCategory.NonSpacingMark
+                select c;
+
+            var cleanStr = new string(chars.ToArray()).Normalize(NormalizationForm.FormC);
+            cleanStr = cleanStr.Replace("Đ", "Dj").Replace("đ", "dj");
+
+            return cleanStr;
+        }
+
+
+        public List<Tuple<String, String>> GetCatiaProductParts(string prodPath)
+        {
+            ProductDocument prodDoc = (ProductDocument)myCATIA.Documents.Read(prodPath);
+            Product prod = prodDoc.Product;
+
+            String path = ((Document)prod.ReferenceProduct.Parent).FullName;
+            Console.WriteLine(path);
+
+            List<Tuple<String, String>> prodParts = new List<Tuple<String, String>>();
+            var prods = prod.Products;
+            Console.WriteLine("Nr of product parts := " + prods.Count);
+            foreach (Product item in prods)
+            {
+                path = ((Document)item.ReferenceProduct.Parent).FullName;
+                Console.WriteLine(path);
+                prodParts.Add(Tuple.Create<string, string>(path, item.get_Name()));
+            }
+            return prodParts;
+        }
+
+
+        public String[]  GetCatiaFileProps(string path, string docType)
+        {
+            Product prod;
+            if (docType == "CATProduct")
+            {
+                prod = ((ProductDocument)myCATIA.Documents.Read(path)).Product;
+            }
+            else
+            {
+                prod = ((MECMOD.PartDocument)myCATIA.Documents.Read(path)).Product;
+            }
+            return new String[] 
+            {
+                prod.get_Name(),
+                prod.get_PartNumber()
+            };
+        }
+
+
+        public bool GetOrCreateInstance()
+        {
+            try
+            {
+                object CATIAinst = Marshal.GetActiveObject(CATIAapp);
+                myCATIA = CATIAinst as INFITF.Application;
+                Console.WriteLine("CATIA instance activated !");
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.Message);
+                try
+                {
+                    Type CATIAType = Type.GetTypeFromProgID(CATIAapp);
+                    object CATIAinst = Activator.CreateInstance(CATIAType);
+                    myCATIA = CATIAinst as INFITF.Application;
+                    Console.WriteLine("CATIA instance created !");
+                }
+                catch (Exception Ex1)
+                {
+                    Console.WriteLine("--- CATIA init ERROR:\n" + Ex1.Message);
+                    return false;
+                }
+            }
+
+            myCATIA.Visible = false;
+            myCATIA.RefreshDisplay = true;
+            myCATIA.DisplayFileAlerts = false;
+
+            return true;
+        }
+
+
+        public void ReadDocs()
+        {
+            INFITF.Documents docs = myCATIA.Documents;
+            Console.WriteLine("Nbr of documents := " + docs.Count.ToString());
+            Console.ReadLine();
+
+            if (docs.Count == 0)
+            {
+                PartDocument partDoc = (PartDocument)docs.Add("Part");
+                Part rootPart = partDoc.Part;
+            }
+
+            string temp = string.Empty;
+            foreach (Document doc in docs)
+            {
+                temp += doc.FullName + '\n';
+            }
+            Console.WriteLine(" --- Components --- \n " + temp);
+        }
+    }
+}
---
+++ CatiaToCGR/Program.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CatiaToCGR
+{
+    class Program
+    {
+        const string confFile = "CatiaToCgr.conf";
+        const string catFilesToProcess = "CATProductsToProcess";
+        //static string rootFld = @"\\stormngodr\Odjeli\TU-SET\KON\Projekti";
+        //static string listFld = @"D:\temp\CATIA\ListaDijelova";
+        //static string cgrFld = @"D:\temp\CATIA\Arhiva za vizualno pretraživanje 3D modela";
+        static string rootFld;
+        static string listFld;
+        static string cgrFld;
+
+        static String[] fileExt = new string[]
+        {
+                "CATProduct",
+                "CATPart"
+        };
+        static CatiaApp catia = new CatiaApp();
+        static decimal Price => 3000;
+        static int myID { get; } = 1000;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Price := " + Price);
+            Console.WriteLine("myID := " + myID);
+            Console.ReadLine();
+            return;
+
+            DateTime start_time, stop_time;
+            TimeSpan elapsed_time;
+            int level = 3;
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-us");
+
+            if (!ReadConfiguration())
+                return;
+
+            List<string> files =
+                GetFiles(level, args.Count() == 0 ? String.Empty : args[0]);
+            if (files == null)
+                return;
+
+            if (!catia.GetOrCreateInstance())
+                return;
+
+            start_time = DateTime.Now;
+            ConvertToCGR(files);
+
+            stop_time = DateTime.Now;
+            elapsed_time = stop_time.Subtract(start_time);
+            Console.WriteLine("Vrijeme obrade : " +
+                elapsed_time.TotalSeconds.ToString("0.00") + " sekunde.\n");
+            try
+            {
+                catia.myCATIA.Quit();
+            }
+            catch (Exception)
+            {
+                if (catia.myCATIA == null)
+                    Console.WriteLine("CATIA has crashed !");
+                else
+                    Console.WriteLine("Cannot quit CATIA  !");
+            }
+            Console.WriteLine("Press any key to continue...");
+            //Console.ReadLine();
+
+        }
+
+
+        static bool ReadConfiguration()
+        {
+            var confPath = Directory.GetCurrentDirectory();
+            if (!File.Exists(confPath + "\\" + confFile))
+            {
+                confPath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                if (!File.Exists(confPath + "\\" + confFile))
+                {
+                    Console.WriteLine("No configuration file --> " + 
+                        confPath + "\\" + confFile + " !");
+                    Console.ReadLine();
+                    return false;
+                }
+            }
+
+            var conf = File.ReadAllLines(confPath + "\\" + confFile)
+                .Where(p => p.Split(new char[] { '=' }).Count() == 2)
+                .Select(s => s.Split(new char[] { '=' }))
+                .Select(s => new Tuple<string, string>
+                (
+                    s[0].ToString().Trim(),
+                    s[1].ToString().Trim()
+                ));
+
+            foreach (var row in conf)
+            {
+                switch (row.Item1)
+                {
+                    case "RootFolder":
+                        rootFld = row.Item2;
+                        break;
+                    case "InOutFolder":
+                        listFld = row.Item2;
+                        break;
+                    case "CatiaFolder":
+                        cgrFld = row.Item2;
+                        break;
+                    default:
+                        Console.WriteLine("No such configuration parameter := " + row.Item1);
+                        break;
+                }
+            }
+            if (rootFld == null)
+            {
+                Console.WriteLine("Parameter RootFolder not set!");
+                return false;
+            }
+            else if (listFld == null)
+            {
+                Console.WriteLine("Parameter InOutFolder not set!");
+                return false;
+            }
+            else if (cgrFld == null)
+            {
+                Console.WriteLine("Parameter CatiaFolder not set!");
+                return false;
+            }
+            else
+                return true;
+        }
+
+        static List<string> GetFiles(int level, string path)
+        {
+            DateTime start_time, stop_time;
+            TimeSpan elapsed_time;
+
+            List<string> files = new List<string>();
+            if (path == String.Empty)
+            {
+                string answer = "N";
+                Console.WriteLine("Do you really want to process all the files in \n'" +
+                    rootFld + "' folder? :=(D/N) ");
+                answer = Console.ReadLine().ToUpper();
+                if (answer == "D")
+                {
+                    Console.WriteLine("Enter max number of lines per file := ");
+                    int nLines = 0;
+                    Int32.TryParse(Console.ReadLine(), out nLines);
+
+                    Console.WriteLine("Scanning CATIA files in level " + level.ToString() + 
+                        " of directory :=\n\t '" + rootFld + "' ...");
+                    start_time = DateTime.Now;
+                    FolderLevelFiles(rootFld, level, fileExt[0], files);
+                    stop_time = DateTime.Now;
+                    elapsed_time = stop_time.Subtract(start_time);
+                    Console.WriteLine("Scan time : " +
+                        elapsed_time.TotalSeconds.ToString("0.00") + " secs.\n");
+
+                    Console.WriteLine("Total number of lines := " + files.Count + ".");
+                    files = files.Distinct().ToList();
+                    Console.WriteLine("Total number of distinct lines := " + files.Count + ".");
+                    Console.WriteLine("Writing files to '" + listFld + "\\" +
+                        catFilesToProcess + ".txt' ...");
+                    StreamWriter sw = new StreamWriter(listFld + "\\" + catFilesToProcess + ".txt");
+                    if (nLines == 0) nLines = files.Count;
+                    int inx = 0;
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        sw.WriteLine(files[i]);
+                        if (i+1 != files.Count && (i+1) % nLines == 0)
+                        {
+                            sw.Close();
+                            inx++;
+                            sw = new StreamWriter(listFld + "\\" + catFilesToProcess +
+                                inx.ToString("D3") + ".txt");
+                        }
+                    }
+                    sw.Close();
+
+                    Console.WriteLine("See output file(s) --> '" + 
+                        listFld + "\\" + catFilesToProcess + "N.txt'.");
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadLine();
+                    return null;
+                }
+                else
+                {
+                    Console.WriteLine("There are no files to process.\n" +
+                        "Press any key to exit...");
+                    Console.ReadLine();
+                    return null;
+                }
+            }
+            else
+            {
+                try
+                {
+                    Console.WriteLine("Reading files from '" + listFld + "\\" +
+                        path + "'...");
+                    files = System.IO.File.ReadAllLines(listFld + "\\" +
+                        path).ToList();
+                    Console.WriteLine(files.Count + " files to be processed...");
+                    return files;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadLine();
+                    return null;
+                }
+            }
+
+            //var fls = System.IO.File.ReadAllLines("ProjectCATProducts.txt");
+            //var bslash = fls.Where(p => p.Count(c => c == '\\') == 9).ToList();
+
+            //Console.WriteLine("Reference number := " + bslash.Count);
+            //Console.WriteLine("Found number := " + files.Count);
+            //Console.ReadLine();
+        }
+
+
+        static void ConvertToCGR(List<string> files)
+        {
+            //string rootFld = @"D:\temp\CATIA\Projekti";
+
+            if (File.Exists(listFld + @"\CATProductList.txt"))
+                File.Delete(listFld + @"\CATProductList.txt");
+            StreamWriter sw = new StreamWriter(listFld + @"\CATProductList.txt", true);
+            //foreach (var file in files)
+            for (int i = 0; i < files.Count; i++)
+            {
+                string prodDocName = catia.ExportToCgr(files[i], cgrFld);
+                if (catia.myCATIA == null)
+                {
+                    Console.WriteLine("CATIA is Down ! --> Product Name := " + files[i]);
+                    if (!catia.GetOrCreateInstance())
+                        return;
+                }
+                Console.WriteLine("Nbr of docs:= " + catia.myCATIA.Documents.Count +
+                    " --> " + files[i]);
+                if (prodDocName != null)
+                {
+                    catia.SaveCgrToProject(prodDocName);
+                    sw.WriteLine(files[i]);
+                    sw.Flush();
+                }
+            }
+            sw.Close();
+        }
+
+
+        static void FolderLevelFiles(string folder, int level, string fileType,
+            List<string> files)
+        {
+            try
+            {
+                var subDirs = Directory.GetDirectories(folder);
+
+                if (level > 1)
+                {
+                    foreach (var dir in subDirs)
+                        FolderLevelFiles(dir, level - 1, fileType, files);
+                }
+                if (level == 1)
+                {
+                    foreach (var file in Directory.GetFiles(folder))
+                    {
+                        if (Path.GetExtension(file) == "." + fileType)
+                            files.Add(file);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
